cec4693a912fbe8fa418e258a2b9cc7194446995
unknown <jaypratap619@gmail.com>
2021-08-03 01:17:55 +0530

    minor changes

--- src/components/ImageListHeader.js
+++ src/components/ImageListHeader.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const ImageListHeader = (props) => {
     return (
         <div className='mt-2 mb-1'>
-            <h3 className='h3 mb-0 '>{props.term}</h3>
+            <h3 className='h3 mb-0 text-capatilize'>{props.term}</h3>
             {
                 props.size === 1 ?
                 <small class="text-muted">{props.size} image has been found</small>
