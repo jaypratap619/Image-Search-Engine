06e183d596a5f66f81be9270e5f66b927d6aa2b3
unknown <jaypratap619@gmail.com>
2021-08-03 01:10:24 +0530

    some changes

--- src/components/ImageListHeader.js
+++ src/components/ImageListHeader.js
@@ -3,18 +3,13 @@ import React from 'react'
 export const ImageListHeader = (props) => {
     return (
         <div className='mt-2 mb-1'>
+            <h3 className='h3 mb-0 '>{props.term}</h3>
             {
-                props.size > 0 &&
-                <>
-                    {
-                        props.size === 1 ?
-                            <small class="text-muted">{props.size} image has been found</small>
-                            :
-                            <small class="text-muted">{props.size} images has been found</small>
-                    }
-                </>
+                props.size === 1 ?
+                <small class="text-muted">{props.size} image has been found</small>
+                :
+                <small class="text-muted">{props.size} images has been found</small>
             }
-            <h3 className='h3 mb-0 text-capitalize'>{props.term}</h3>
         </div>
     )
 }
